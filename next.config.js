e0a99dca34f6109184e682bb7f08906d302be82b
lethdz <dzung.leThanh@outlook.com>
2024-10-14 02:21:50 +0700

    Common/Adding lint and prettier to projects

--- next.config.js
+++ next.config.js
@@ -2,14 +2,15 @@
 const nextConfig = {
   experimental: {
     typedRoutes: true,
-		caseSensitiveRoutes: true,
-		appDocumentPreloading: true,
-		optimizeServerReact: true
+    caseSensitiveRoutes: true,
+    appDocumentPreloading: true,
+    optimizeServerReact: true,
   },
-	typescript: {
-		ignoreBuildErrors: false,
-		tsconfigPath: "tsconfig.json"
-	}
+  typescript: {
+    ignoreBuildErrors: false,
+    tsconfigPath: 'tsconfig.json',
+  },
+  basePath: '/src',
 };
 
 module.exports = nextConfig;
